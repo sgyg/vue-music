52c4dbbef09072570867d5f52d425ed2b0b2b343
sgy <l>
2020-09-11 08:00:43 +0800

    first

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,32 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    redirect: "/recommend"
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/recommend",
+    name: "Recommend",
+    component: () => import("../components/recommend/recommend")
+  },
+  {
+    path: "/singer",
+    name: "Singer",
+    component: () => import("../components/singer/singer")
+  },
+  {
+    path: "/rank",
+    name: "Rank",
+    component: () => import("../components/rank/rank")
+  },
+  {
+    path: "/search",
+    name: "Search",
+    component: () => import("../components/search/search")
   }
 ];
 
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const { resolve } = require("path");
+
+module.exports = {
+  chainWebpack: config => {
+    // 设置快捷路径， @ 表示 'src' ，components 表示 'src/components'
+    config.resolve.alias
+      .set("@", resolve("./src"))
+      .set("components", resolve("./src/components"))
+      .set("common", resolve("./src/common"))
+      .set("base", resolve("./src/base"))
+      .set("api", resolve("./src/api"));
+  }
+};
